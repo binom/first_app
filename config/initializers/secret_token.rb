cb2ad48236717e68e3bad36105d5619e61674690
binom <binomod@gmail.com>
2011-06-27 14:06:52 +0400

    install commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'de1162d13e2bfdfc2768e7929189f6db058dd94a5dd32db42fd414d13d24c26bd7810b4548474d789b82c1d3e24333a766d7cd88cdc8e804860893d297aeb185'
